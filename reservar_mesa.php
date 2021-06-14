9ecd87290021f5e84f70a95311d8d2fc17e48915
Felipe Timana <luistimanafelipe@gmail.com>
2021-06-14 17:46:01 -0500

    agregado php para registro de reserva

--- reservar_mesa.php
+++ reservar_mesa.php
@@ -3,35 +3,6 @@ include("db_conect.php");
 
 $mysql_conn = db_connect();
 
-
-
-
-
-
-
-
-
-$hora_actual = date("Y-m-d H:i:s");
-$time = $_POST["time"];
-$time2 = date("H:i:s", strtotime($time));
-$consulta = "
-    INSERT INTO 
-    reservas set
-    fecha = '".$_POST["date"]."',
-    hora = '".$_POST["time"]."',
-    numeropersonas = '".$_POST["people"]."',
-    mensaje = '".$_POST["message"]."',
-    fechaenvio = '".$hora_actual."',
-    clientes_id = '".$_POST["name"]."',
-    mesas_id = '3'
-    
-    ";
-
-mysqli_query($mysql_conn, $consulta);
-
-
-echo $consulta;
-
 $_POST["name"];
 $_POST["email"];
 $_POST["phone"];
@@ -40,5 +11,88 @@ $_POST["time"];
 $_POST["people"];
 $_POST["message"];
 
-header('Location: http://localhost/SIN-Project/index.html')
+
+$hora_actual = date("Y-m-d H:i:s");
+$time = $_POST["time"];
+$time2 = date("H:i:s", strtotime($time));
+
+//Validacion del cliente
+function get_id_cliente($nombre_cliente, $mysql_conn) 
+{
+
+    $consulta_cliente = "
+    SELECT idclientes 
+    FROM clientes
+    where nombre = '".$nombre_cliente."'
+    ";
+
+    $resultado_cliente = mysqli_query($mysql_conn, $consulta_cliente);
+    $id_cliente_validation = mysqli_fetch_array($resultado_cliente);
+    return $id_cliente_validation;
+}
+
+
+//Agregar cliente o recuperar id
+if(!empty(get_id_cliente($_POST["name"], $mysql_conn))) {
+
+    $idcliente_registrado_array = get_id_cliente($_POST["name"], $mysql_conn);
+    $idcliente_registrado = $idcliente_registrado_array[0];
+} else {
+
+    $consulta_cliente_insert = "
+    INSERT INTO 
+    clientes set
+    nombre = '".$_POST["name"]."',
+    email = '".$_POST["email"]."',
+    celular = '".$_POST["phone"]."'
+    ";
+    mysqli_query($mysql_conn, $consulta_cliente_insert);
+
+    $idcliente_registrado_array = get_id_cliente($_POST["name"], $mysql_conn);
+    $idcliente_registrado = $idcliente_registrado_array[0];
+}
+
+
+//Mesas
+
+function get_id_mesa($capacidad_mesas_get, $mysql_conn) 
+{
+
+    $consulta_mesa = "
+    SELECT idmesas 
+    FROM mesas
+    where capacidad = '".$capacidad_mesas_get."'
+    ";
+
+    $resultado_mesa = mysqli_query($mysql_conn, $consulta_mesa);
+    $id_mesa_validation = mysqli_fetch_array($resultado_mesa);
+    return $id_mesa_validation[0];
+}
+
+$id_mesa_reservacion = get_id_mesa($_POST["people"], $mysql_conn);
+
+
+//Reservas 
+
+$consulta = "
+    INSERT INTO 
+    reservas set
+    fecha = '".$_POST["date"]."',
+    hora = '".$_POST["time"]."',
+    numeropersonas = '".$_POST["people"]."',
+    mensaje = '".$_POST["message"]."',
+    fechaenvio = '".$hora_actual."',
+    clientes_id = '".$idcliente_registrado."',
+    mesas_id = '".$id_mesa_reservacion."'
+    
+    ";
+
+mysqli_query($mysql_conn, $consulta);
+
+
+
+
+
+
+// header('Location: http://localhost/SIN-Project/index.html')
 ?>
